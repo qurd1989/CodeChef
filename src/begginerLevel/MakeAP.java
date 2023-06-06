2ad603c893e38fda1ffc303483514a5448904435
Qurd1989 <mashaedu@gmail.com>
2023-06-06 14:10:16 -0400

    Beginner level codes!

---
+++ src/begginerLevel/MakeAP.java
@@ -0,0 +1,29 @@
+package begginerLevel;
+
+import java.util.Scanner;
+
+public class MakeAP {
+
+    public static void main(String[] args) {
+
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            if((a + b) % 2 == 0) {
+
+                System.out.println((a + b) / 2);
+            }else
+
+                System.out.println(-1);
+
+        }
+    }
+}
---
+++ src/begginerLevel/Monoply.java
@@ -0,0 +1,8 @@
+package begginerLevel;
+
+public class Monoply {
+
+    public static void main(String[] args) {
+
+    }
+}
