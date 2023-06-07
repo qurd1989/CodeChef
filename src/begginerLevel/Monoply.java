47edabec558d455f24087245265c640aa4a1331b
Qurd1989 <mashaedu@gmail.com>
2023-06-07 10:38:31 -0400

    Beginner level codes!

--- src/begginerLevel/Monoply.java
+++ src/begginerLevel/Monoply.java
@@ -1,8 +1,28 @@
 package begginerLevel;
 
+import java.util.Scanner;
+
 public class Monoply {
 
     public static void main(String[] args) {
 
-    }
+
+        Scanner sc= new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            int p = sc.nextInt();
+            int q = sc.nextInt();
+            int r = sc.nextInt();
+            int s = sc.nextInt();
+
+            if ((p + q + r) < s || (p + q + s) <r || (p + r + s) < q || (q + r + s) <p) {
+                System.out.println("Yes");
+            }else
+                System.out.println("No");
+
+        }
+        }
 }
---
+++ src/begginerLevel/ProblemsInYourToDoList.java
@@ -0,0 +1,21 @@
+package begginerLevel;
+
+import java.util.Scanner;
+
+public class ProblemsInYourToDoList {
+
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            int p = sc.nextInt();
+            int q = sc.nextInt();
+
+
+        }
+    }
+}
