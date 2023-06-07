4aeb719cffb96b58678d3675b6c1bc4b6b28561c
Qurd1989 <mashaedu@gmail.com>
2023-06-07 11:40:31 -0400

    Beginner level codes!

--- src/begginerLevel/ProblemsInYourToDoList.java
+++ src/begginerLevel/ProblemsInYourToDoList.java
@@ -5,22 +5,21 @@ import java.util.Scanner;
 public class ProblemsInYourToDoList {
 
     public static void main(String[] args) {
-
         Scanner sc= new Scanner(System.in);
 
         int t = sc.nextInt();
-         int count = 0;
+
         while (t-- > 0) {
+            int count = 0;
+            int toDo = sc.nextInt();
+            int arr[] = new int[toDo];
 
-         int toDo = sc.nextInt();
-         int arr[] = new int[toDo];
-
-         for (int i = 0; i < arr.length; i++) {
-             arr[i] = sc.nextInt();
-             if (arr[i] >= 1000) {
-                 count++;
-             }
-         }
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = sc.nextInt();
+                if (arr[i] >= 1000) {
+                    count++;
+                }
+            }
 
             System.out.println(count);
 
