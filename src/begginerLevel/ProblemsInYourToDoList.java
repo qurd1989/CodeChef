aeb333d60ad11fa5433feb3317230cdddbfa2200
Qurd1989 <mashaedu@gmail.com>
2023-06-07 11:39:01 -0400

    Beginner level codes!

--- src/begginerLevel/ProblemsInYourToDoList.java
+++ src/begginerLevel/ProblemsInYourToDoList.java
@@ -9,14 +9,20 @@ public class ProblemsInYourToDoList {
         Scanner sc= new Scanner(System.in);
 
         int t = sc.nextInt();
-
+         int count = 0;
         while (t-- > 0) {
 
-            System.out.println();
+         int toDo = sc.nextInt();
+         int arr[] = new int[toDo];
 
-            int p = sc.nextInt();
-            int q = sc.nextInt();
+         for (int i = 0; i < arr.length; i++) {
+             arr[i] = sc.nextInt();
+             if (arr[i] >= 1000) {
+                 count++;
+             }
+         }
 
+            System.out.println(count);
 
         }
     }
