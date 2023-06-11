f0090ca258346d853b06de9611bbc1331840f5f9
Qurd1989 <mashaedu@gmail.com>
2023-06-10 22:37:30 -0400

    Beginner level codes!

--- src/begginerLevel/IntegerThatSumToTheSameValue.java
+++ src/begginerLevel/IntegerThatSumToTheSameValue.java
@@ -1,8 +1,21 @@
 package begginerLevel;
 
+import java.util.Scanner;
+
 public class IntegerThatSumToTheSameValue {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            int j = n - i;
+            count++;
+
+        }
+        System.out.println(count);
     }
 }
---
+++ src/begginerLevel/SastaSharkTank.java
@@ -0,0 +1,18 @@
+package begginerLevel;
+
+import java.util.Scanner;
+
+public class SastaSharkTank {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+         -
+    }
+}
