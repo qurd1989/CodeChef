63d98e6aba0eabd8a84c2eb89d264714883b4932
Qurd1989 <mashaedu@gmail.com>
2023-06-13 22:16:58 -0400

    Beginner level codes!

---
+++ src/begginerLevel/MiniumCarsRequired.java
@@ -0,0 +1,21 @@
+package begginerLevel;
+
+import java.util.Scanner;
+
+public class MiniumCarsRequired {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t--> 0) {
+
+            int n = sc.nextInt();
+
+            int total = (int)Math.ceil(n/4);
+
+        }
+    }
+}
