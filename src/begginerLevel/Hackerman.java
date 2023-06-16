96d39db6d97a60fb59419b63cd76186f18bff119
Qurd1989 <mashaedu@gmail.com>
2023-06-15 22:24:43 -0400

    Beginner level codes!

--- src/begginerLevel/Hackerman.java
+++ src/begginerLevel/Hackerman.java
@@ -12,9 +12,33 @@ public class Hackerman {
 
         while (t-- > 0) {
 
-            int x = sc.nextInt();
+            int a = sc.nextInt();
             int y = sc.nextInt();
 
+            int sum = a + y;
+
+            if (!isPrime(sum)) {
+                System.out.println("alice");
+            }else
+                System.out.println("Bob");
+
         }
+    }
+    public static boolean isPrime(int n) {
+
+        boolean flag = false;
+
+
+        if (n <= 1) {
+            return flag = false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+
+            if (n % i == 0) {
+                return flag = true;
+            }
+        }
+        return flag;
     }
 }
