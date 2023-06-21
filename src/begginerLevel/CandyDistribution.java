0de892becd8d716fe17b71d97783a088fa9d656e
Qurd1989 <mashaedu@gmail.com>
2023-06-21 11:26:29 -0400

    Beginner level codes!

--- src/begginerLevel/CandyDistribution.java
+++ src/begginerLevel/CandyDistribution.java
@@ -1,7 +1,24 @@
 package begginerLevel;
 
+import java.util.Scanner;
+
 public class CandyDistribution {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while(t--> 0) {
+
+            int n = sc.nextInt(); // N is number of Candies
+            int m = sc.nextInt(); // M is number of Friends candies should be distributed.
+
+            if (n % m== 0 && n % 2==0) {
+                System.out.println("Yes");
+
+            }else
+                System.out.println("No");
+        }
     }
 }
