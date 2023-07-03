aa1467bbc9f5be31f2a9a4e36f33bbce565b63db
Qurd1989 <mashaedu@gmail.com>
2023-07-03 17:38:40 -0400

    Beginner level codes!

---
+++ src/begginerLevel/ImportantPagesOnCodeChef.java
@@ -0,0 +1,20 @@
+package begginerLevel;
+
+import java.util.Scanner;
+
+public class ImportantPagesOnCodeChef {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+
+            int a = sc.nextInt();   //
+            int b = sc.nextInt();   //
+
+
+
+
+    }
+}
