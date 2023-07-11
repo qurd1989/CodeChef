62171b942741812aee51717baec15b42e3cf2ef0
Qurd1989 <mashaedu@gmail.com>
2023-07-11 15:10:13 -0400

    Beginner level codes!

---
+++ src/begginerLevel/ChefAndRaces.java
@@ -0,0 +1,25 @@
+package begginerLevel;
+
+import java.util.Scanner;
+
+public class ChefAndRaces {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            int x = sc.nextInt();   //
+            int y = sc.nextInt();   //
+            int a = sc.nextInt();   //
+            int b = sc.nextInt();   //
+
+
+
+
+        }
+    }
+}
---
+++ src/begginerLevel/Nameless.java
@@ -0,0 +1,17 @@
+package begginerLevel;
+
+import java.util.Scanner;
+
+public class Nameless {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+
+        int max = (x > 10) ? 3*x : 4 * x;
+
+    }
+}
