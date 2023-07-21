4d8ae92552222cf6eb0ee584040828fd51fd8cf4
Qurd1989 <mashaedu@gmail.com>
2023-07-21 09:24:28 -0400

    Beginner level codes!

---
+++ src/begginerLevel/SmallFactorial.java
@@ -0,0 +1,32 @@
+package begginerLevel;
+
+import java.util.Scanner;
+
+public class SmallFactorial {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            int n = sc.nextInt();
+
+/*
+
+            int factorial = 1;
+            for (int i = 1; i <=n; i++) {
+
+                factorial *=i;
+
+
+            }
+            System.out.println(factorial);
+*/
+
+
+        }
+    }
+}
