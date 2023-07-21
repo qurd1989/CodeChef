1945cf38bf49b99aa791c4eef720a4bdaa473300
Qurd1989 <mashaedu@gmail.com>
2023-07-21 09:09:31 -0400

    Beginner level codes!

---
+++ src/begginerLevel/PresentsForCheffina.java
@@ -0,0 +1,20 @@
+package begginerLevel;
+
+import java.util.Scanner;
+
+public class PresentsForCheffina {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0){
+
+            int n = sc.nextInt();
+
+            System.out.println(n - (n/5));
+        }
+    }
+}
