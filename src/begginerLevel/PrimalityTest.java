a8d46b8dc5b94fa067dc0ced17cb1a957c2a352d
Qurd1989 <mashaedu@gmail.com>
2023-08-07 09:27:47 -0400

    Beginner level codes!

--- src/begginerLevel/PrimalityTest.java
+++ src/begginerLevel/PrimalityTest.java
@@ -11,21 +11,26 @@ public class PrimalityTest {
         while (t-- > 0) {
             int n = s.nextInt();
 
-
+            if (n < 2) {
+                System.out.println("no");
+                continue;
+            }
             boolean falg = false;
-            for (int i = 0; i < n /2; i++) {
+            for (int i = 2; i < n; i++) {
 
-                if (n % 2 == 0) {
+                if (n % i == 0) {
                     falg = true;
 
                 }
             }
 
             if (!falg) {
-                System.out.println("No");
-            }else
-                System.out.println("Yes");
-            }
+                System.out.println("yes");
+            } else
+                System.out.println("no");
         }
     }
+}
+
+
 
