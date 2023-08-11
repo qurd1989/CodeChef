2acc6ac3c22b291c544d83c6c2dc9297bee0d631
Qurd1989 <mashaedu@gmail.com>
2023-08-10 22:37:50 -0400

    Beginner level codes!

--- src/begginerLevel/HardestProblemBet.java
+++ src/begginerLevel/HardestProblemBet.java
@@ -8,15 +8,18 @@ public class HardestProblemBet {
 
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
 
-        if (a < b && a < c) {
-            System.out.println("Alice");
-        }else if (a > b && b < c) {
-            System.out.println("Bob");
-        }else
-            System.out.println("Draw");
+            if (a < b && a < c) {
+                System.out.println("Draw");
+            } else if (a > b && b < c) {
+                System.out.println("Bob");
+            } else
+                System.out.println("Alice");
+        }
     }
 }
