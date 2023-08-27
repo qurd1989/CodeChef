40ad5ce0ef8be3e56143c5843ac6c1ba23b1a48c
Qurd1989 <mashaedu@gmail.com>
2023-08-26 22:20:38 -0400

    Beginner level codes!

--- src/begginerLevel/GradeltheSteel.java
+++ src/begginerLevel/GradeltheSteel.java
@@ -13,8 +13,8 @@ public class GradeltheSteel {
         for (int i =  0; i < k; i++) {
 
             int h = sc.nextInt();
-            int c = sc.nextInt();
-            int t = sc.nextInt();
+            float c = sc.nextFloat();
+            float t = sc.nextFloat();
 
             if (h > 50 && c < 0.7 && t > 5600) {
                 System.out.println(10);
@@ -28,6 +28,20 @@ public class GradeltheSteel {
                 System.out.println(6);
             }else
                 System.out.println(5);
+
+        }
+
+        boolean alive = true;
+        if (alive) {
+            while (true) {
+                System.out.println("NEVER GIVE UP");
+                if (!alive) {
+                    break;
+                }
+            }
+        } else {
+            System.out.println("You are dead");
         }
     }
+
 }
