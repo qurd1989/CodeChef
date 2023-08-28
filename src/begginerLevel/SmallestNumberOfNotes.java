55e52092a85f2ec8159b3ac9f8d5ed7417625257
Qurd1989 <mashaedu@gmail.com>
2023-08-27 21:33:42 -0400

    Beginner level codes!

--- src/begginerLevel/SmallestNumberOfNotes.java
+++ src/begginerLevel/SmallestNumberOfNotes.java
@@ -8,7 +8,10 @@ public class SmallestNumberOfNotes {
 
         Scanner sc=new Scanner(System.in);
         int t=sc.nextInt();
-        while(t-->0){
+
+        int x = solve(sc);
+        System.out.println(x);
+       /* while(t-->0){
             int n=sc.nextInt();
             int a =n%100;
             int b= a%50;
@@ -16,8 +19,24 @@ public class SmallestNumberOfNotes {
             int d= c%5;
             int e= d%2;
 
-            System.out.println((n/100)+(a/50)+(b/10)+(c/5)+(d/2)+e);
+            System.out.println((n/100)+(a/50)+(b/10)+(c/5)+(d/2)+e);*/
 
         }
+        public static  int solve(Scanner sc) {
+
+        int denominations[] = {100, 50, 20, 5, 2,1};
+
+        int n = sc.nextInt();
+        int output = 0;
+
+        for (int i = 0; i < denominations.length; i++) {
+            if ( n >= denominations[i]) {
+
+                output += n / denominations[i];
+                n %= denominations[i];
+
+            }
+        }
+            return output;
     }
 }
