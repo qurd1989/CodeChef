5148f9d038de1e6cb1be8d06c33fb674c34ca975
Qurd1989 <mashaedu@gmail.com>
2023-09-01 22:52:45 -0400

    Beginner level codes!

--- src/DSAPreparetion1/ThreeNumberExpression.java
+++ src/DSAPreparetion1/ThreeNumberExpression.java
@@ -1,4 +1,26 @@
 package DSAPreparetion1;
 
+import java.security.spec.RSAOtherPrimeInfo;
+import java.util.Scanner;
+
 public class ThreeNumberExpression {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            long c = sc.nextLong();
+
+            if ((a + b) == c || (a + c) == b || (b + c) == a) {
+                System.out.println("Yes");
+            }else
+                System.out.println("No");
+        }
+    }
 }
