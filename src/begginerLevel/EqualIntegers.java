5d0c73c85258896b8217be570229cf8af4dd75e9
Qurd1989 <mashaedu@gmail.com>
2023-09-03 22:05:02 -0400

    Beginner level codes!

--- src/begginerLevel/EqualIntegers.java
+++ src/begginerLevel/EqualIntegers.java
@@ -5,18 +5,26 @@ import java.util.Scanner;
 public class EqualIntegers {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        int t = sc.nextInt();
-
-        for (int i = 0; i < t; i++) {
-
-            long a = sc.nextLong();
-            long b = sc.nextLong();
-
-            int difference = (int) Math.abs(a-b);
-            System.out.println(difference);
-
+        Scanner read = new Scanner(System.in);
+        int t = read.nextInt();
+        for(int i=0; i<t; i++){
+            int x = read.nextInt();
+            int y = read.nextInt();
+            int count = 0;
+            if(x==y){
+                System.out.println(0);
+            }
+            if(x<y){
+                System.out.println((y-x)/1);
+            }
+            else if(x>y){
+                if((x-y)%2==1){
+                    System.out.println(2+((x-y)/2));
+                }
+                else{
+                    System.out.println((x-y)/2);
+                }
+            }
         }
     }
 }
