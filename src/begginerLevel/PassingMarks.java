53a68bc63e9fd8f13f008279f61d228bea95ee25
Qurd1989 <mashaedu@gmail.com>
2023-09-11 19:28:04 -0400

    Beginner level codes!

---
+++ src/begginerLevel/PassingMarks.java
@@ -0,0 +1,26 @@
+package begginerLevel;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class PassingMarks {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++)
+        {
+            int n = sc.nextInt();
+            int x = sc.nextInt();
+
+            int arr[] = new int[n];
+            for (int k = 0;  k < arr.length; k++){
+
+                arr[k] = sc.nextInt();
+
+            }
+        }
+    }
+}
