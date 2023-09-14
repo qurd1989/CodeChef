dec202bee80f08c4ea2d42b11ca38f2a582a4b39
Qurd1989 <mashaedu@gmail.com>
2023-09-13 22:26:53 -0400

    Beginner level codes!

--- src/DSAPreparetion1/LuckyNumber.java
+++ src/DSAPreparetion1/LuckyNumber.java
@@ -1,9 +1,29 @@
 package DSAPreparetion1;
 
+import java.util.Scanner;
+
 public class LuckyNumber {
 
     public static void main(String[] args) {
 
-        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        int countPower = 0;
+        for (int i = 0 ; i < t; i++) {
+            long n =sc.nextInt();
+
+            while (n % 2 == 0) {
+                    n/=2;
+                    countPower++;
+                }
+            }
+
+            if (countPower % 2 == 0){
+                System.out.println(1);
+            }else
+                System.out.println(0);
+        }
+
     }
-}
+
