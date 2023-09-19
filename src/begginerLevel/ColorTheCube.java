55a4c2ea3ad66b70afbc3825b86f8ebe9ad0bafc
Qurd1989 <mashaedu@gmail.com>
2023-09-18 23:34:11 -0400

    Beginner level codes!

---
+++ src/begginerLevel/ColorTheCube.java
@@ -0,0 +1,26 @@
+package begginerLevel;
+
+import java.util.Scanner;
+
+public class ColorTheCube {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            long sum = 0;
+            int arr[] = new int[n];
+            for (int j = 0; j <arr.length; j++) {
+                arr[j] = sc.nextInt();
+
+                arr[j] += sum;
+
+            }
+             long result = sum * (long)(Math.ceil(n)/2);
+            System.out.println(result);
+        }
+    }
+}
