716688ed92a5fef1b1923e2b1c39a0ca86edadc7
Qurd1989 <mashaedu@gmail.com>
2023-09-24 15:39:55 -0400

    Beginner level codes!

---
+++ src/begginerLevel/BellsOfPligrimage.java
@@ -0,0 +1,21 @@
+package begginerLevel;
+
+import java.util.Scanner;
+
+public class BellsOfPligrimage {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt(); // N bells  must be ranged
+            int x = sc.nextInt(); // x is the first ring
+            int k = sc.nextInt(); // k bells have been ranged
+            int p = sc.nextInt(); //  P is initial mana
+
+
+        }
+    }
+}
---
+++ src/prepareDSAInterviews/ReverseTheLinkedList.java
@@ -0,0 +1,23 @@
+
+
+class Node {
+    int val;
+    Node next;
+
+    Node(int val) {
+        this.val = val;
+    }
+}
+
+
+    class ReverseTheLinkedList {
+        public static void main(String[] args) {
+
+            Node head = new Node(2)
+            Node temp = new Node(7);
+
+            temp.next = head
+
+        }
+    }
+
