cbfc0686ac7af77f5fd9b2a9f2cffffde568ecb4
Qurd1989 <mashaedu@gmail.com>
2023-09-25 16:35:48 -0400

    Beginner level codes!

---
+++ src/begginerLevel/InsideTheStadium.java
@@ -0,0 +1,15 @@
+package begginerLevel;
+
+import java.util.Scanner;
+
+public class InsideTheStadium {
+    public static void main(String[] args) {
+
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        while (t-- > 0) {
+            int n = s.nextInt();
+
+        }
+    }
+}
