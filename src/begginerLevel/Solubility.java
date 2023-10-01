3b9db92108e2ccba090efccc5ef6766f0b2cc1a6
Qurd1989 <mashaedu@gmail.com>
2023-10-01 12:36:52 -0400

    Beginner level codes!

---
+++ src/begginerLevel/Solubility.java
@@ -0,0 +1,20 @@
+package begginerLevel;
+
+import java.util.Scanner;
+
+public class Solubility {
+
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+        int t= sc.nextInt();
+        while(t-- >0)
+        {
+            int x= sc.nextInt();
+            int a= sc.nextInt();
+            int b= sc.nextInt();
+            System.out.println((a+(100-x)*b)*10);
+        }
+        sc.close();
+    }
+}
