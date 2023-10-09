4d81e7f54f4076228dd67891cfe4bdd116ddc966
Qurd1989 <mashaedu@gmail.com>
2023-10-08 20:28:45 -0400

    Beginner level codes!

---
+++ src/begginerLevel/AkashAndMissingClass.java
@@ -0,0 +1,15 @@
+package begginerLevel;
+
+import java.util.Scanner;
+
+public class AkashAndMissingClass {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            System.out.println(n/ 6);
+        }
+    }
+}
