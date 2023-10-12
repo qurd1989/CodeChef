dec5892c082d2c71c0cb5df2675ccd05fe89d315
Qurd1989 <mashaedu@gmail.com>
2023-10-12 13:28:52 -0400

    Beginner level codes!

---
+++ src/begginerLevel/CarRange.java
@@ -0,0 +1,18 @@
+package begginerLevel;
+
+import java.util.Scanner;
+
+public class CarRange {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int p = scanner.nextInt();
+            int m = scanner.nextInt();
+            int v = scanner.nextInt();
+
+        }
+    }
+}
