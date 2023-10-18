a8e7850e72a43f7c5b30d0dd486530bf6bd5cc22
Qurd1989 <mashaedu@gmail.com>
2023-10-17 22:14:31 -0400

    Beginner level codes!

---
+++ src/begginerLevel/Marathon.java
@@ -0,0 +1,29 @@
+package begginerLevel;
+
+import java.util.Scanner;
+
+public class Marathon {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int dDays = sc.nextInt();
+            int dKilometer = sc.nextInt();
+            int marathonOne = sc.nextInt();
+            int marathonTwo = sc.nextInt();
+            int marathonThree = sc.nextInt();
+
+            int totalKilometer = dDays * dKilometer;
+
+            if (totalKilometer >= 42) {
+                System.out.println(marathonThree);
+            } else if (totalKilometer <= 21 && 42 > totalKilometer) {
+                System.out.println(marathonTwo);
+            }else if (totalKilometer >= 10 && totalKilometer < 21){
+                System.out.println(marathonOne);
+            }else
+                System.out.println(0);
+        }
+    }
+}
