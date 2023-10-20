84491e68018f2458a4f048757d7d37d038e06a0f
Qurd1989 <mashaedu@gmail.com>
2023-10-19 21:16:28 -0400

    Beginner level codes!

---
+++ src/begginerLevel/TwoRooks.java
@@ -0,0 +1,23 @@
+package begginerLevel;
+
+import java.util.Scanner;
+
+public class TwoRooks {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while (t--> 0){
+
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+            if (x1 == x2 || y1 == y2){
+                System.out.println("Yes");
+            }else
+                System.out.println("No");
+        }
+    }
+}
