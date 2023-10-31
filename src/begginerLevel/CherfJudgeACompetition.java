5406aa23532cda0062302b481ecddc578cabda4d
Qurd1989 <mashaedu@gmail.com>
2023-10-31 15:09:42 -0400

    Beginner level codes!

---
+++ src/begginerLevel/CherfJudgeACompetition.java
@@ -0,0 +1,18 @@
+package begginerLevel;
+
+import java.util.Scanner;
+
+public class CherfJudgeACompetition {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int x = sc.nextInt();
+            int arr[] = new int[n];
+            var r = 3;
+        }
+    }
+}
