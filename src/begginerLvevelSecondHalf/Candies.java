26129c7bf3bab2cf50d0d4191380b9e12f10dc9b
Qurd1989 <mashaedu@gmail.com>
2023-11-17 23:35:09 -0500

    Beginner level from 1000 To 2000!

---
+++ src/begginerLvevelSecondHalf/Candies.java
@@ -0,0 +1,16 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+
+public class Candies {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t =sc.nextInt();
+        while (t--> 0){
+            int n =sc.nextInt();
+            int firstTypeCandies[] = new int[n *2];
+
+        }
+
+    }
+}
