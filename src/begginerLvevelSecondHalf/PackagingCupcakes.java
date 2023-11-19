0d67ed0044f8c442f1b532b01f0cc91e14d67d0a
Qurd1989 <mashaedu@gmail.com>
2023-11-19 15:14:13 -0500

    Beginner level from 1000 To 2000!

---
+++ src/begginerLvevelSecondHalf/PackagingCupcakes.java
@@ -0,0 +1,15 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+
+public class PackagingCupcakes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t--> 0){
+            int n = sc.nextInt();
+            System.out.println((n / 2) + 1);
+        }
+
+    }
+}
