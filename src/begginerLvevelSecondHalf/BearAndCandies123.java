832d99cecdc8ede2f113cf3060a4e16c00f253e9
Qurd1989 <mashaedu@gmail.com>
2023-11-22 11:26:21 -0500

    Beginner level from 1000 To 2000!

---
+++ src/begginerLvevelSecondHalf/BearAndCandies123.java
@@ -0,0 +1,31 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+
+public class BearAndCandies123 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t--> 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int i= 1;
+            String ans = "";
+
+            while (true){
+                if (i * i > a) {
+                    ans = "Bob";
+                    break;
+                }
+                if ((i * i) +1 > b) {
+                    ans = "Limak";
+                    break;
+                }
+                i++;
+            }
+
+            System.out.println(ans);
+        }
+
+    }
+}
