d0294520a3124d77edba2d65ea1103739d67d2ab
Qurd1989 <mashaedu@gmail.com>
2023-12-06 22:50:22 -0500

    Beginner level from 1000 To 2000!

--- src/begginerLvevelSecondHalf/OddPairs.java
+++ src/begginerLvevelSecondHalf/OddPairs.java
@@ -1,8 +1,16 @@
 package begginerLvevelSecondHalf;
 
+import java.util.Scanner;
+
 public class OddPairs {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t--> 0){
+            long n = sc.nextInt();
+            System.out.println((n*n)/2);
+        }
     }
 }
