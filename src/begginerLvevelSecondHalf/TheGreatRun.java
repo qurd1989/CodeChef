68eb7dde078665b976806e87b3c09e4598b1e2de
Qurd1989 <mashaedu@gmail.com>
2023-12-19 21:57:32 -0500

    Beginner level from 1000 To 2000!

---
+++ src/begginerLvevelSecondHalf/TheGreatRun.java
@@ -0,0 +1,25 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+
+public class TheGreatRun {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t--> 0){
+            int n =sc.nextInt();
+            int k = sc.nextInt();
+
+            int max = 0;
+            int  girls[] = new int[n];
+            for (int i = 0; i< n; i++){
+                girls[i] = sc.nextInt();
+            }
+            for (int i = 0; i < n; i++) {
+
+            }
+        }
+    }
+}
