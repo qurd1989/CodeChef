e6670c90662fa8d432d8c4b37e3a9578bef1d4c2
Qurd1989 <mashaedu@gmail.com>
2023-12-20 20:42:56 -0500

    Beginner level from 1000 To 2000!

--- src/begginerLvevelSecondHalf/PrimeGenerator.java
+++ src/begginerLvevelSecondHalf/PrimeGenerator.java
@@ -1,31 +1,30 @@
 package begginerLvevelSecondHalf;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PrimeGenerator {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-         int n = sc.nextInt();
-         int x = sc.nextInt();
-         int arr[] = new int[n];
-         for (int i= 0; i < n; i++) {
-             arr[i] = sc.nextInt();
-         }
-             int ans[] = solve(arr, x);
-             if (ans.length > 0)
-             {
-                 for (int num : ans){
-                     System.out.println(num);
-                 }
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int ans[] = solve(arr, x);
+        if (ans.length > 0) {
+            for (int num : ans) {
+                System.out.println(num);
+            }
 
-                 }else
-                 System.out.println("IMPOSSIBLE");
-
-
+        } else
+            System.out.println("IMPOSSIBLE");
          }
 
     public  static int[] solve(int []arr, int x){
+        Arrays.sort(arr);
         int right = arr.length-1;
         int left = 0;
         while (right > left) {
