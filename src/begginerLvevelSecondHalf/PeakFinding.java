731a228cf5df1ced785b1099a3de2e1e1b015cb6
Qurd1989 <mashaedu@gmail.com>
2023-12-24 23:05:58 -0500

    Beginner level from 1000 To 2000!

---
+++ src/begginerLvevelSecondHalf/PeakFinding.java
@@ -0,0 +1,23 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+
+public class PeakFinding {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t--> 0){
+            int n = sc.nextInt();
+            int mountains[] = new int[n];
+            int max = 0;
+            for(int i = 0; i< n; i++){
+                mountains[i] = sc.nextInt();
+                if (mountains[i] > max){
+                    max = mountains[i];
+                }
+            }
+            System.out.println(max);
+        }
+    }
+}
