5e594803bce30156990e26782c4ef532a9e07e52
Qurd1989 <mashaedu@gmail.com>
2024-01-01 20:58:01 -0500

    Beginner level from 1000 To 2000!

---
+++ src/begginerLvevelSecondHalf/FriendsMeetup.java
@@ -0,0 +1,21 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+
+public class FriendsMeetup {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t--> 0){
+
+            int x1 = sc.nextInt();
+            int x2 = sc.nextInt();
+
+            if (x1 == x2 || x1 > x2){{
+                System.out.println("YES");
+            }}else
+                System.out.println("NO");
+
+        }
+    }
+}
