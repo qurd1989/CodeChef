1220eca27fdea2da23f394e5a62296a888d1faf5
Qurd1989 <mashaedu@gmail.com>
2024-01-12 23:50:03 -0500

    Beginner level from 1000 To 2000!

---
+++ src/begginerLvevelSecondHalf/ChefVsDoof.java
@@ -0,0 +1,28 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+
+public class ChefVsDoof {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            boolean flag = true;
+            for (int i = 0; i < n; i++){
+                arr[i] = sc.nextInt();
+            }
+            for (int i = 0; i < n; i++){
+                if (arr[i] % 2 == 0){
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag){
+                System.out.println("YES");
+            }else
+                System.out.println("NO");
+        }
+    }
+}
