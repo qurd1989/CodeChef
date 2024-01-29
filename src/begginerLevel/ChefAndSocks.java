353ba9017efd5df83ff48d2c1d56a28d7ef73ce0
Qurd1989 <mashaedu@gmail.com>
2024-01-28 20:17:42 -0500

    Beginner level from 1000 To 2000!

---
+++ src/begginerLevel/ChefAndSocks.java
@@ -0,0 +1,20 @@
+package begginerLevel;
+
+import java.util.Scanner;
+
+public class ChefAndSocks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+         int jacketCost = sc.nextInt();
+         int sockCost = sc.nextInt();
+         int buyingPower = sc.nextInt();
+         int left = buyingPower - jacketCost;
+
+         if ((left / sockCost)%2 ==0){
+             System.out.println("Lucky Chef");
+         }else
+             System.out.println("Unlucky Chef");
+
+    }
+}
