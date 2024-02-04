1c8e50bfc63a2cf4c7975ec9fd15e009b3e4d84e
Qurd1989 <mashaedu@gmail.com>
2024-02-04 12:56:16 -0500

    Beginner level from 1000 To 2000!

--- src/begginerLvevelSecondHalf/DevuAndArray.java
+++ src/begginerLvevelSecondHalf/DevuAndArray.java
@@ -5,7 +5,30 @@ import java.util.Scanner;
 public class DevuAndArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-
+        int n = sc.nextInt();
+        int q = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int min = arr[0];
+        for (int i = 0; i < n; i++){
+            if (min > arr[i]){
+                min = arr[i];
+            }
+        }
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++){
+            if (max < arr[i]){
+                max = arr[i];
+            }
+        }
+        for (int i = 0; i < q; i++){
+            int t =sc.nextInt();
+            if (t <= max && t >= min){
+                System.out.println("Yes");
+            }else
+                System.out.println("No");
+        }
     }
 }
