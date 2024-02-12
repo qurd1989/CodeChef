616cd664dd297c2517507da5300b8a31d8204376
Qurd1989 <mashaedu@gmail.com>
2024-02-11 23:36:33 -0500

    Beginner level from 1000 To 2000!

--- src/begginerLvevelSecondHalf/BuyingSweets.java
+++ src/begginerLvevelSecondHalf/BuyingSweets.java
@@ -8,6 +8,28 @@ public class BuyingSweets {
         int t = sc.nextInt();
         while (t--> 0){
 
+            int n = sc.nextInt();
+            int x = sc.nextInt();
+            int arr[] = new int[n];
+            int sumOfNotes = 0;
+            for (int i = 0; i < arr.length; i++){
+                arr[i] = sc.nextInt();
+                sumOfNotes += arr[i];
+            }
+            int a = sumOfNotes/x;
+            int count=0;
+            for(int j=0;j<n;j++){
+                if(sumOfNotes-arr[j]>=(x*a)){
+                    count++;
+                    break;
+                }
+            }
+            if(count>0){
+                System.out.println(-1);
+            }
+            else{
+                System.out.println(a);
+            }
         }
     }
 }
