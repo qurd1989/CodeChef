6a9189793ba36d03bb426a4576ccb6ab3714a07a
Qurd1989 <mashaedu@gmail.com>
2024-02-15 15:06:33 -0500

    Beginner level from 1000 To 2000!

---
+++ src/begginerLevel/AnotherGoodString.java
@@ -0,0 +1,19 @@
+package begginerLevel;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class AnotherGoodString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t--> 0){
+            int n = sc.nextInt();
+            int q = sc.nextInt();
+            String s = sc.next();
+            int count = 0;
+            Map<Character, Integer> hs = new HashMap<>();
+        }
+    }
+}
