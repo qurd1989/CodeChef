2477524adf5554cf726e85b71348556676c29606
Qurd1989 <mashaedu@gmail.com>
2024-02-16 22:33:41 -0500

    Beginner level from 1000 To 2000!

---
+++ src/begginerLvevelSecondHalf/FitInDataType.java
@@ -0,0 +1,18 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+
+public class FitInDataType {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t =sc.nextInt();
+        while(t-->0){
+            int n =sc.nextInt();
+            int x = sc.nextInt();
+           if (n > x){
+               System.out.println(x);
+           }else
+               System.out.println(x % (n + 1));
+        }
+    }
+}
