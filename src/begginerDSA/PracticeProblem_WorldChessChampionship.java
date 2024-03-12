4a7102abc9d568ca58f469aaeae05165fe99a13e
Qurd1989 <mashaedu@gmail.com>
2024-03-11 23:32:52 -0400

    Beginner level from 1000 To 2000!

---
+++ src/begginerDSA/PracticeProblem_WorldChessChampionship.java
@@ -0,0 +1,17 @@
+package begginerDSA;
+
+import java.util.Scanner;
+
+public class PracticeProblem_WorldChessChampionship {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int t = read.nextInt();
+        for(int i=0; i<t; i++)
+        {
+            int x = read.nextInt();
+            String s = read.next();
+            // Update the code below to solve the problem
+
+        }
+    }
+}
