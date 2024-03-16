12f6e52c4237b61318280f37bd4ffbeca2591462
Qurd1989 <mashaedu@gmail.com>
2024-03-15 23:39:21 -0400

    Beginner level from 1000 To 2000!

---
+++ src/begginerDSA/PracticeProblemConstructN.java
@@ -0,0 +1,15 @@
+package begginerDSA;
+
+import java.util.Scanner;
+
+public class PracticeProblemConstructN {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+            int n  = sc.nextInt();
+
+            System.out.println((n + 4)/5);
+        }
+    }
+
