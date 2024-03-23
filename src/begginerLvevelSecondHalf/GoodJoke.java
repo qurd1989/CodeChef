b510ee1b8fd682862fcbeb35b8b622f720eb0f12
Qurd1989 <mashaedu@gmail.com>
2024-03-22 23:25:59 -0400

    Beginner level from 1000 To 2000!

--- src/begginerLvevelSecondHalf/GoodJoke.java
+++ src/begginerLvevelSecondHalf/GoodJoke.java
@@ -7,30 +7,23 @@ public class GoodJoke {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while (t-- > 0){
+        while (t-- > 0) {
             int n = sc.nextInt();
-            String s = "123";
-            System.out.println(fibonacci(n));
-            enterLimit_Price(s);
+             int a[] = new int[n];
+             int b[] = new int[n];
+             for (int i = 0; i < n; i++){
+                 a[i] =  sc.nextInt();
+                 b[i] = sc.nextInt();
+             }
+             int res = 0;
+             for (int i = 1; i <=n; i++){
+                 res ^= i;
+             }
+            System.out.println(res);
         }
+
     }
-    public static int fibonacci(int n) {
-        if (n <= 1) {
-            return n;
-        }
-
-        return (fibonacci(n - 1)+ fibonacci(n - 2));
-    }
-
-
     public static void enterLimit_Price(String limit){
-        int limitprice = Integer.valueOf(limit);
-        double lastPrice = 123.45;
-        double price = (limitprice * lastPrice)/ 100;
-        //double limitPrice = Math.round(price * 10.0) /10.0;
-       // long finalPrice = Math.round(limitPrice * 100);
-        String  updatedPrice = String.valueOf(price);
-        System.out.println(updatedPrice);
 
     }
 
