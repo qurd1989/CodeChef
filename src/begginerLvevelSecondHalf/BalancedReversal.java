293ccace50089ee18846170e8cf809542e7e4436
Qurd1989 <mashaedu@gmail.com>
2024-03-24 22:18:48 -0400

    Beginner level from 1000 To 2000!

---
+++ src/begginerLvevelSecondHalf/BalancedReversal.java
@@ -0,0 +1,34 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+
+public class BalancedReversal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0){
+            int n = sc.nextInt();
+            String binaryString = sc.next();
+            String onesAndZerosAfterReversion = "";
+            int countZeros = 0;
+            int countOnes = 0;
+            for (int i = 0; i < n; i++){
+                if (binaryString.charAt(i) == '1') {
+                    countOnes++;
+                }else
+                    countZeros++;
+            }
+            while (countZeros != 0){
+                onesAndZerosAfterReversion +="0";
+                countZeros--;
+            }
+            while (countOnes != 0){
+                onesAndZerosAfterReversion +="1";
+                countOnes--;
+            }
+
+            System.out.println(onesAndZerosAfterReversion);
+        }
+
+    }
+}
