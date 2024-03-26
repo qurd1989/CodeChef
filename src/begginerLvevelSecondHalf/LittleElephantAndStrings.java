a7cba3b7bf85591ee1b6293b9e6dc7f9de60f79e
Qurd1989 <mashaedu@gmail.com>
2024-03-25 23:20:43 -0400

    Beginner level from 1000 To 2000!

---
+++ src/begginerLvevelSecondHalf/LittleElephantAndStrings.java
@@ -0,0 +1,36 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+
+public class LittleElephantAndStrings {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        String s[] = new String[n];
+        for (int i = 0; i < n; i++) {
+            s[i] = sc.next();
+        }
+        for (int i = 0; i < k; i++){
+            String subString = sc.next();
+            int flag = 0;
+            if (subString.length() >= 47) {
+                flag = 1;
+            } else {
+                for (int j = 0; j < n; j++) {
+                    if (subString.contains(String.valueOf(s[j]))) {
+                        flag = 1;
+                        break;
+                    }
+                }
+            }
+            if (flag == 1) {
+                System.out.println("Good");
+            } else {
+                System.out.println("Bad");
+            }
+        }
+    }
+}
+
+
