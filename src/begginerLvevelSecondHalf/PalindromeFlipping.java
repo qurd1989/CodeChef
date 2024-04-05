4778d0386d35de6879e19da8543e39b3c80f864f
Qurd1989 <mashaedu@gmail.com>
2024-04-04 23:33:22 -0400

    Beginner level from 1000 To 2000!

--- src/begginerLvevelSecondHalf/PalindromeFlipping.java
+++ src/begginerLvevelSecondHalf/PalindromeFlipping.java
@@ -9,8 +9,24 @@ public class PalindromeFlipping {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         while(t--> 0){
+            int n = sc.nextInt();
             String s = sc.next();
-            System.out.println(reverseStringStack(s));
+            int zerosCounts = 0;
+            int onesCounts = 0;
+            for (int i = 0; i < s.length(); i++){
+                if (s.charAt(i) == '1'){
+                    onesCounts++;
+                }else
+                    zerosCounts++;
+            }
+            if (s.length()%2 == 0) {
+                System.out.println("Yes");
+            } else {
+                if (onesCounts % 2 == 0 && zerosCounts % 2 == 0) {
+                    System.out.println("Yes");
+                } else
+                    System.out.println("No");
+            }
         }
     }
 
