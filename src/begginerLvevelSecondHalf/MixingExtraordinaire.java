f7ebffc08df01e80fda20259721da9bec1f339c3
Qurd1989 <mashaedu@gmail.com>
2024-04-07 11:21:20 -0400

    Beginner level from 1000 To 2000!

--- src/begginerLvevelSecondHalf/MixingExtraordinaire.java
+++ src/begginerLvevelSecondHalf/MixingExtraordinaire.java
@@ -8,7 +8,19 @@ public class MixingExtraordinaire {
         int t = sc.nextInt();
         while(t--> 0){
             int n = sc.nextInt();
-
+             long drinks[] = new long[n];
+             long sum = 0;
+             for (int i = 0; i < n; i++){
+                 drinks[i] = sc.nextInt();
+             }
+             for (int i = 0; i < n; i++){
+                 long combination = 0;
+                 for (int j = i + 1; j < n; j++){
+                     combination= drinks[i] * drinks[j];
+                     sum += combination;
+                 }
+             }
+            System.out.println(sum);
         }
     }
 }
