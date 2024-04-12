f8d17d34f5d0e7f0858545216d79fa08ab1d2b26
Qurd1989 <mashaedu@gmail.com>
2024-04-11 23:50:36 -0400

    Beginner level from 1000 To 2000!

---
+++ src/begginerLvevelSecondHalf/ChefAndGroups.java
@@ -0,0 +1,28 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class ChefAndGroups {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t--> 0) {
+            String s = sc.next();
+            int n = s.length();
+            int count = 0;
+            int i = 0;
+            while (i< n) {
+                if (s.charAt(i) == '0'){
+                    i++;
+                }else
+                    count++;
+                    i++;
+                    while (i < n && s.charAt(i) == '1'){
+                        i++;
+                    }
+            }
+            System.out.println(count);
+        }
+    }
+}
