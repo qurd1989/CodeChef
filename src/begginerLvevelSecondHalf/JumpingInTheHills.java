0e4231ef67150fd4aa31b8305ce8de1a68787df8
Qurd1989 <mashaedu@gmail.com>
2024-05-31 23:35:33 -0400

    new commits

---
+++ src/begginerLvevelSecondHalf/JumpingInTheHills.java
@@ -0,0 +1,42 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+
+public class JumpingInTheHills {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while(t-->0){
+            int n = in.nextInt();
+            int u = in.nextInt();
+            int d = in.nextInt();
+            int[] arr = new int[n];
+            for(int i=0;i<n;i++){
+                arr[i] = in.nextInt();
+            }
+            boolean para = true;
+            int index = 1;
+            for(int i=1;i<n;i++){
+                if(arr[i]==arr[i-1]){
+                    index++;
+                }else if(arr[i]>arr[i-1]){
+                    if(arr[i]-arr[i-1]<=u){
+                        index++;
+                    }else{
+                        break;
+                    }
+                }else if(arr[i]<arr[i-1]){
+                    if(arr[i-1]-arr[i]<=d){
+                        index++;
+                    }else if(para){
+                        index++;
+                        para = false;
+                    }else{
+                        break;
+                    }
+                }
+            }
+            System.out.println(index);
+        }
+    }
+}
