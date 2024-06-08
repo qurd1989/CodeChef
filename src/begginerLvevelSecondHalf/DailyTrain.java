4405391283faa9454768212dc89c5318139f3915
Qurd1989 <mashaedu@gmail.com>
2024-06-07 23:19:04 -0400

    new commit

---
+++ src/begginerLvevelSecondHalf/DailyTrain.java
@@ -0,0 +1,12 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+
+public class DailyTrain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+
+    }
+}
