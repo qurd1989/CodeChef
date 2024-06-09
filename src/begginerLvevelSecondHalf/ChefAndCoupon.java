7c4c0aa7e34d438a82043d83151cb6641617de04
Qurd1989 <mashaedu@gmail.com>
2024-06-08 23:05:48 -0400

    new commit

--- src/begginerLvevelSecondHalf/ChefAndCoupon.java
+++ src/begginerLvevelSecondHalf/ChefAndCoupon.java
@@ -7,7 +7,32 @@ public class ChefAndCoupon {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         while(t-- > 0){
+            int d =sc.nextInt();
+            int c = sc.nextInt();
+            int a[] = new int[3];
+            int b[] = new int[3];
+            int aSUm = 0;
+            int bSum = 0;
+            for (int i = 0; i < 3; i++){
+                a[i] = sc.nextInt();
+                aSUm += a[i];
+            }
+            for (int i = 0; i <3; i++){
+                b[i] = sc.nextInt();
+                bSum += b[i];
+            }
+            int day1BillWithDelivery = aSUm + d;
+            int day2BillWithDelivery = bSum + d;
+            int totalBillWithoutCoupon = day1BillWithDelivery + day2BillWithDelivery;
+            int x = (aSUm < 150) ? day1BillWithDelivery : aSUm;
+            int y = (bSum < 150) ? day2BillWithDelivery : bSum;
 
+            int totalBillWithCoupon = x + y + c;
+
+            if(totalBillWithCoupon >= totalBillWithoutCoupon)
+                System.out.println("NO");
+            else
+                System.out.println("YES");
         }
     }
 }
