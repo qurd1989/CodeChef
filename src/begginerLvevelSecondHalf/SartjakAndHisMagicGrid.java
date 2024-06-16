8b6f7e95cd760fb564f429de563f8b7164b2984f
Qurd1989 <mashaedu@gmail.com>
2024-06-16 18:48:01 -0400

    new commit

---
+++ src/begginerLvevelSecondHalf/SartjakAndHisMagicGrid.java
@@ -0,0 +1,20 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+
+public class SartjakAndHisMagicGrid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t--> 0){
+            int n = sc.nextInt();
+
+            for (int i = 0; i < n; i++){
+                for (int j = 0; j < n; j++){
+                    System.out.println("1 ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
