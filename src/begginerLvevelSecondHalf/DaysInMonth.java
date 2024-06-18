6e994fd136089c1c106463c6fce83d2758d95366
Qurd1989 <mashaedu@gmail.com>
2024-06-17 23:20:54 -0400

    new commit

--- src/begginerLvevelSecondHalf/DaysInMonth.java
+++ src/begginerLvevelSecondHalf/DaysInMonth.java
@@ -7,12 +7,34 @@ public class DaysInMonth {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         while(t--> 0){
-            int w = sc.nextInt();
-            String s = sc.next();
+            int n = sc.nextInt();
+            String str = sc.next();
+            int startDay = -1;
+            if(str.equals("mon")){startDay = 0;}
+            else if(str.equals("tues")){startDay = 1;}
+            else if(str.equals("wed")){startDay = 2;}
+            else if(str.equals("thurs")){startDay = 3;}
+            else if(str.equals("fri")){startDay = 4;}
+            else if(str.equals("sat")){startDay = 5;}
+            else if(str.equals("sun")){startDay = 6;}
 
+            int count = 0;
+            int[] arr = new int[7];
+
+            for(int i=startDay; i<7; i=(i+1)%7){
+                arr[i]++;
+                count++;
+
+                if(count==n){break;}
+            }
+
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
         }
-
     }
+
     public static String toLowerCase(String str){
         int n = str.length();
         StringBuilder sb = new StringBuilder();
@@ -27,8 +49,6 @@ public class DaysInMonth {
 
         return new String(sb);
     }
-
-
     public static boolean isPerfectSquare(long n) {
         long low = 1;
         long high = n;
@@ -45,5 +65,4 @@ public class DaysInMonth {
         }
         return false;
     }
-
 }
