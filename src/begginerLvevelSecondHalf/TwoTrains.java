9e4e7561c23fdd3c645302e3883a6ddc2697c963
Qurd1989 <mashaedu@gmail.com>
2024-06-22 23:16:17 -0400

    new commit

---
+++ src/begginerLvevelSecondHalf/TwoTrains.java
@@ -0,0 +1,35 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+
+public class TwoTrains {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t--> 0){
+            int n = sc.nextInt();
+            int sp = 0, sm = 0;
+            while (n-->0){
+                int c = sc.nextInt();
+                sm = Math.max(sm, c);
+            }
+            System.out.println(sp + sp);
+        }
+    }
+    public static int[] generateFibonacciNumbers(int n) {
+      if (n == 0){
+          return new int[0];
+      }
+      int arr[] = new int[n];
+      if (n >= 1){
+          arr[0] = 0;
+      }
+      if (n >= 2){
+          arr[1] = 1;
+      }
+      for (int i = 2; i < n; i++){
+          arr[i] = arr[i -1] + arr[i - 2];
+      }
+      return arr;
+    }
+}
