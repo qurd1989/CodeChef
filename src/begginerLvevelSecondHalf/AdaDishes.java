e97a1167c2aa7fa62ca08846248735200771871d
Qurd1989 <mashaedu@gmail.com>
2024-06-30 20:47:12 -0400

    new commit

---
+++ src/begginerLvevelSecondHalf/AdaDishes.java
@@ -0,0 +1,15 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+
+public class AdaDishes {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t--> 0) {
+            int n = sc.nextInt();
+
+        }
+
+    }
+}
