26b4f669f5dfcc02eb1aecc52b5d79f64a011cd6
Qurd1989 <mashaedu@gmail.com>
2024-07-06 21:45:19 -0400

    new commit

---
+++ src/begginerLvevelSecondHalf/ClosestVowels.java
@@ -0,0 +1,19 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+
+public class ClosestVowels {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t--> 0){
+            int n = sc.nextInt();
+            String s = sc.next();
+
+        }
+    }
+    private  static boolean isVowel(char ch){
+        return (ch == 'a' ||ch == 'i' || ch == 'o' || ch == 'e' || ch == 'o');
+    }
+
+}
