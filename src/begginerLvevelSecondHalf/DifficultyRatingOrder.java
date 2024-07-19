7a3eb5528609246e937be9ea8f79a9ce3d9aa3f6
Qurd1989 <mashaedu@gmail.com>
2024-07-19 15:54:37 -0400

    new commit

---
+++ src/begginerLvevelSecondHalf/DifficultyRatingOrder.java
@@ -0,0 +1,29 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+
+public class DifficultyRatingOrder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while(t--> 0) {
+            int n = scanner.nextInt();
+            int[] d = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                d[i] = scanner.nextInt();
+            }
+            boolean isIncreasing = false;
+            for (int i = 0; i< n; i++){
+                if (d[i] > d[i +1]){
+                    isIncreasing = true;
+                    break;
+                }
+            }
+            if (isIncreasing){
+                System.out.println("No");
+            }else
+                System.out.println("Yes");
+        }
+    }
+}
