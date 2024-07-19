f389a5d26f43264873a48c1115c680ee9d8bf1fd
Qurd1989 <mashaedu@gmail.com>
2024-07-18 23:29:35 -0400

    new commit

---
+++ src/begginerLvevelSecondHalf/LargestAndSecondLargest.java
@@ -0,0 +1,21 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+
+public class LargestAndSecondLargest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while (t--> 0) {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            int largest = 0;
+            int secondLargest =0;
+            for (int i = 0; i < n; i++){
+                arr[i] = sc.nextInt();
+            }
+        }
+
+    }
+}
