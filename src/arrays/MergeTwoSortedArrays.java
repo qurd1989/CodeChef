f31a45bea6309c4b6d8d999f68922abca9320e2e
Qurd1989 <mashaedu@gmail.com>
2024-07-24 23:19:54 -0400

    new commit

--- src/arrays/MergeTwoSortedArrays.java
+++ src/arrays/MergeTwoSortedArrays.java
@@ -36,4 +36,37 @@ public class MergeTwoSortedArrays {
             System.out.print(value + " ");
         }
     }
+    public double probabilityOfHeads(double[] coinProbabilities, int target) {
+
+        // Initialize an array to store the probabilities of getting exactly 'i' heads after tossing 'j' coins.
+        // f[i] represents the probability of getting exactly 'i' heads.
+        double[] probabilities = new double[target + 1];
+
+        // Base case: the probability of getting 0 heads (all tails) is initially 1.
+        probabilities[0] = 1;
+
+        // Iterate over each coin probability.
+        for (double coinProbability : coinProbabilities) {
+
+            // Iterate backwards over the possible numbers of heads.
+            // This is to prevent that the updating of f[j] affects the updating of f[j+1].
+            for (int j = target; j >= 0; --j) {
+
+                // Each time a coin is tossed, the probability of getting 'j' heads is updated:
+                // 1. The probability of getting 'j' heads without the current coin (probability of tails
+                //    of the current coin is multiplied by the previous 'j' heads probability).
+                probabilities[j] *= (1 - coinProbability);
+
+                // 2. If j is more than 0, update the probability by adding:
+                //    the probability of getting 'j - 1' heads after the previous tosses and getting heads
+                //    this time (current coin's probability of heads is multiplied by the previous 'j-1' heads probability).
+                if (j > 0) {
+                    probabilities[j] += coinProbability * probabilities[j - 1];
+                }
+            }
+        }
+
+        // The final result is the probability of getting exactly 'target' heads after tossing all coins.
+        return probabilities[target];
+    }
 }
