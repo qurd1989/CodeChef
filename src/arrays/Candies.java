8c0a64570a354a58360508f483c00d675558aac3
Qurd1989 <mashaedu@gmail.com>
2024-07-26 23:13:00 -0400

    new commit

--- src/arrays/Candies.java
+++ src/arrays/Candies.java
@@ -1,5 +1,6 @@
 package arrays;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -8,16 +9,13 @@ public class Candies {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         for (int i = 0; i < t; i++) {
+            int k = sc.nextInt();
             int n = sc.nextInt();
-            int[] a = new int[n * 2];
-            for (int j = 0; j < a.length; j++) {
+            int[] a = new int[n];
+            for (int j = 0; j < n; j++) {
                 a[j] = sc.nextInt();
             }
-            if (isSplitable(a, n)) {
-                System.out.println("Yes");
-            } else {
-                System.out.println("No");
-            }
+            System.out.println(subarraySum(a,k));
         }
     }
     public static boolean isSplitable(int arr[], int n) {
@@ -34,4 +32,17 @@ public class Candies {
         }
         return true;
     }
+    public static int subarraySum(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap< Integer, Integer > map = new HashMap < > ();
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
 }
+
--- src/arrays/Workers.java
+++ src/arrays/Workers.java
@@ -45,11 +45,40 @@ public class Workers {
         }
         return true;
     }
-    public static boolean isPalindorme(String s){
+    public static boolean isPalindrome1(String s){
         int n = s.length();
         if (n <= 1){
             return true;
         }else
-            return s.charAt(0) == s.charAt(n - 1) && isPalindorme(s.substring(1, n - 1));
+            return s.charAt(0) == s.charAt(n - 1) && isPalindrome1(s.substring(1, n - 1));
+    }
+    public static boolean isPalindrome(String str){
+        int n = str.length();
+        for (int i = 0; i < n/2; i++){
+            if (str.charAt(i) != str.charAt(n-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static int[] generateFibonacciNumbers(int n) {
+        int arr[] = new int[n];
+        if(n > 0){
+            arr[0] =0;
+        }
+        if(n > 1){
+            arr[1] = 1;
+        }
+        fib(arr, n, 2);
+        return arr;
+    }
+    public static void fib(int arr[], int n, int currentIndex){
+        if(currentIndex >= n){
+            return;
+        }
+        arr[currentIndex] = arr[currentIndex-1] + arr[currentIndex -2];
+        fib(arr, n, currentIndex + 1);
+
     }
 }
+
