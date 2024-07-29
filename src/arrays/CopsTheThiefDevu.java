afbd870ab4dc813b4dfa1b5df35b5382e9cb987b
Qurd1989 <mashaedu@gmail.com>
2024-07-28 21:45:26 -0400

    new commit

---
+++ src/arrays/CopsTheThiefDevu.java
@@ -0,0 +1,18 @@
+package arrays;
+import java.util.*;
+public class CopsTheThiefDevu {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();;
+        while (t -- > 0){
+            int m = sc.nextInt(); // m is size of array
+            int x = sc.nextInt();// x house per min
+            int y = sc.nextInt();// y is cop search per min
+            int[] arr = new int[m];
+            for (int i = 0; i < m; i++){
+                arr[i] = sc.nextInt();
+            }
+
+        }
+    }
+}
---
+++ src/begginerLvevelSecondHalf/TheManCode.java
@@ -0,0 +1,15 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+
+public class TheManCode {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            System.out.println((n  + 1)/ 2 + " " + (n+2)/3);
+        }
+    }
+}
