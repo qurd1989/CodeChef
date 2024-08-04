327ff63fde4af1a39734be34dfd4bc49b39a90f1
Qurd1989 <mashaedu@gmail.com>
2024-08-03 22:32:33 -0400

    new commit

--- src/begginerLvevelSecondHalf/EvenSumSubArray.java
+++ src/begginerLvevelSecondHalf/EvenSumSubArray.java
@@ -7,24 +7,24 @@ public class EvenSumSubArray {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         while(t--> 0) {
-            int n = sc.nextInt();
-            int arr[] = new int[n];
-            for (int i = 0; i < n; i++){
-                arr[i] = sc.nextInt();
-            }
-            int sum = 0;
-            for (int i = 0; i < n; i++){
-                for (int j = i; j < n; j++){
-                    int sum1 = 0;
-                    for (int k = i; k <= j; k++){
-                        sum1 += arr[k];
-                    }
-                    if (sum1 % 2 == 0){
-                        sum++;
-                    }
-                }
-            }
-            System.out.println(sum);
+//            int n = sc.nextInt();
+//            int arr[] = new int[n];
+//            for (int i = 0; i < n; i++) {
+//                arr[i] = sc.nextInt();
+//            }
+            String s = sc.next();
+            System.out.println(isValid(s) ? "YES" : "NO");
         }
     }
+
+    public static boolean isValid(String s){
+        int n = s.length();
+        for(int i = 0; i < n; i++){
+            if (s.charAt(i) != s.charAt(n - i - 1)){
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
