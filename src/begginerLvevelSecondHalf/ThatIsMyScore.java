437e1b139333b5a6512578f8b82283c39aaed2f2
Qurd1989 <mashaedu@gmail.com>
2024-08-05 16:41:14 -0400

    fdfdfd

--- src/begginerLvevelSecondHalf/ThatIsMyScore.java
+++ src/begginerLvevelSecondHalf/ThatIsMyScore.java
@@ -9,7 +9,23 @@ public class ThatIsMyScore {
         Scanner sc = new Scanner(System.in);
         int t =sc.nextInt();
         while (t-->0){
-
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++){
+                int p = sc.nextInt();
+                int s = sc.nextInt();
+                if (p < 9){
+                    if (arr[p-1] < s){
+                        arr[p-1] = s;
+                    }
+                }
+            }
+            Arrays.sort(arr);
+            int sum = 0;
+            for (int i = 0; i < 8; i++){
+                sum += arr[i];
+            }
+            System.out.println(sum);
         }
     }
 }
