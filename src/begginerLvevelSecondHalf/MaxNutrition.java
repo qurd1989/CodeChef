c15be409af23c1ac8787fb08bd8c22db1b494405
Qurd1989 <mashaedu@gmail.com>
2024-08-23 22:32:16 -0400

    new commit

--- src/begginerLvevelSecondHalf/MaxNutrition.java
+++ src/begginerLvevelSecondHalf/MaxNutrition.java
@@ -1,5 +1,7 @@
 package begginerLvevelSecondHalf;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 public class MaxNutrition {
     public static void main(String[] args) {
@@ -15,8 +17,28 @@ public class MaxNutrition {
             for(int i = 0; i < n; i++){
                 nutritionValues[i] = sc.nextInt();
             }
-            int maxNutrition = 0;
+            System.out.println(maxNutrition(typeFruits, nutritionValues));
 
-                 }
+        }
     }
+    public static int maxNutrition(int typeFruits[], int nutritionValues[]){
+        int n = typeFruits.length;
+        int maxNutrition = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < n; i++) {
+            int type  = typeFruits[i];
+            int nutrition = nutritionValues[i];
+            if(!map.containsKey(type) || map.get(type) < nutrition){
+                map.put(type, nutrition);
+            }
+        }
+        for(int value : map.values()) {
+            if (value > 0) {
+                maxNutrition += value;
+            }
+        }
+        return maxNutrition;
+    }
+
 }
