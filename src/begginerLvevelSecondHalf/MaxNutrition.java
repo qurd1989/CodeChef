88fed441324039ea27957eecf28cad6eb1c21e8d
Qurd1989 <mashaedu@gmail.com>
2024-08-23 22:00:10 -0400

    new commit

---
+++ src/begginerLvevelSecondHalf/MaxNutrition.java
@@ -0,0 +1,22 @@
+package begginerLvevelSecondHalf;
+
+import java.util.Scanner;
+public class MaxNutrition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t--> 0){
+            int n = sc.nextInt();
+            int typeFruits[] = new int[n];
+            int nutritionValues[] = new int[n];
+            for(int i = 0; i < n; i++){
+                typeFruits[i] = sc.nextInt();
+            }
+            for(int i = 0; i < n; i++){
+                nutritionValues[i] = sc.nextInt();
+            }
+            int maxNutrition = 0;
+
+                 }
+    }
+}
