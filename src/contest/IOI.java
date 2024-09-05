efb4d80608e8581d2cb96556cf61c3795c490b7a
Qurd1989 <mashaedu@gmail.com>
2024-09-04 22:48:56 -0400

    new commit

---
+++ src/contest/IOI.java
@@ -0,0 +1,18 @@
+package contest;
+
+import java.util.Scanner;
+
+public class IOI {
+
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        if(x > 8){
+            System.out.println("NO");
+        }else{
+            System.out.println("YES");
+        }
+
+    }
+}
--- src/contest/RatioByTwo.java
+++ src/contest/RatioByTwo.java
@@ -7,24 +7,23 @@ import java.util.*;
 
 public class RatioByTwo {
     static CustomPrintStream out;
-    static void solve(FastReader cin,CustomPrintStream ps){
-        int t=cin.nextInt();
 
-        while (t-->0) {
-            int x=cin.nextInt();
-            int y=cin.nextInt();
+    static void solve(FastReader cin, CustomPrintStream ps) {
+        int t = cin.nextInt();
 
-            int operations=0;
-            if(x>=2*y || y>=2*x){
-            }
-            else{
-                if(x == y){
-                    operations+= x - (x/2);
-                }
-                else{
-                    int ld= x-(y/2);
-                    int rd = y- (x/2);
-                    operations = Math.min(ld,rd);
+        while (t-- > 0) {
+            int x = cin.nextInt();
+            int y = cin.nextInt();
+
+            int operations = 0;
+            if (x >= 2 * y || y >= 2 * x) {
+            } else {
+                if (x == y) {
+                    operations += x - (x / 2);
+                } else {
+                    int ld = x - (y / 2);
+                    int rd = y - (x / 2);
+                    operations = Math.min(ld, rd);
                 }
             }
             cout(operations);
@@ -35,28 +34,62 @@ public class RatioByTwo {
         FastReader in = new FastReader();
         solve(in, out);
     }
-    static void cout(Object x){System.out.println(x);}
+    static void cout(Object x) {
+        System.out.println(x);
+    }
     static class FastReader {
-        BufferedReader br; StringTokenizer st;
-        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
         String next() {
             while (st == null || !st.hasMoreElements()) {
-                try { st = new StringTokenizer(br.readLine()); } catch (IOException e) { e.printStackTrace(); }
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
             return st.nextToken();
         }
-        int nextInt() { return Integer.parseInt(next()); }
-        long nextLong() { return Long.parseLong(next()); }
-        double nextDouble() { return Double.parseDouble(next()); }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
         String nextLine() {
             String str = "";
-            try { str = br.readLine(); } catch (IOException e) { e.printStackTrace(); }
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             return str;
         }
     }
+
     static class CustomPrintStream extends PrintStream {
-        public CustomPrintStream(OutputStream out) { super(out); }
-        void yes(){this.println("YES");}
-        void no(){this.println("NO");}
+        public CustomPrintStream(OutputStream out) {
+            super(out);
+        }
+
+        void yes() {
+            this.println("YES");
+        }
+
+        void no() {
+            this.println("NO");
+        }
     }
 }
