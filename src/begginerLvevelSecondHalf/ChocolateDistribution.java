f587aae15d430e46fae4181fbb59072ed94b8dc0
Qurd1989 <mashaedu@gmail.com>
2024-09-19 21:29:47 -0400

    new commit

--- src/begginerLvevelSecondHalf/ChocolateDistribution.java
+++ src/begginerLvevelSecondHalf/ChocolateDistribution.java
@@ -4,6 +4,10 @@ public class ChocolateDistribution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
+        while(t--> 0) {
+            int n= sc.nextInt();
 
+            System.out.println(1+" "+1+" "+(n-2));
+        }
     }
 }
