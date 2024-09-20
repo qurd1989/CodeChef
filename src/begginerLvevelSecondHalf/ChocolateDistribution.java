0b6f70a331871e0ef9e5280d15e61db3c1f95160
Qurd1989 <mashaedu@gmail.com>
2024-09-19 21:27:14 -0400

    new commit

---
+++ src/begginerLvevelSecondHalf/ChocolateDistribution.java
@@ -0,0 +1,9 @@
+package begginerLvevelSecondHalf;
+import java.util.Scanner;
+public class ChocolateDistribution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+    }
+}
