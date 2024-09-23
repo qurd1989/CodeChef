c59a7295f95d74bbf4fde2b7ae245573812cb17e
Qurd1989 <mashaedu@gmail.com>
2024-09-22 20:35:29 -0400

    new commit

--- src/contest/RangeMinimze.java
+++ src/contest/RangeMinimze.java
@@ -27,8 +27,10 @@ public class RangeMinimze {
             int option1 = A[n-1] - A[2];
             // Remove the largest two elements (keep elements from A[0] to A[N-3])
             int option2 = A[n-3] - A[0];
+            System.out.println("option2: "+option2);
             // Remove the smallest one and the largest one (keep elements from A[1] to A[N-2])
             int option3 = A[n-2] - A[1];
+            System.out.println("option1: "+option1);
 
             // Find the minimum of the three options
             int result = Math.min(option1, Math.min(option2, option3));
