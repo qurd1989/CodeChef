2e31e12b80b654842ee491f8aeb5220b99a26088
Qurd1989 <mashaedu@gmail.com>
2025-03-18 23:51:25 -0400

    afads

--- src/arrays/RemoveBadElements.java
+++ src/arrays/RemoveBadElements.java
@@ -5,7 +5,21 @@ import java.util.Scanner;
 public class RemoveBadElements {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(removeBadElements(arr, n));
 
-
+    }
+    public static int removeBadElements(int[] arr, int n) {
+        int count = 0;
+        for (int i = 0; i < n; i++){
+            if (arr[i] >= 0){
+                arr[count++] = arr[i];
+            }
+        }
+        return count;
     }
 }
